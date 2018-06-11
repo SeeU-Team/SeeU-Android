056af719bcc7f09867f5a35e4eead5d41657347d
thomasfouan <thomasfouan@hotmail.fr>
2018-06-11 23:12:40 +0200

    add websocket support

--- app/src/main/java/com/seeu/chat/ChatActivity.java
+++ app/src/main/java/com/seeu/chat/ChatActivity.java
@@ -6,13 +6,18 @@ import android.support.annotation.Nullable;
 import android.view.View;
 import android.widget.EditText;
 
+import com.google.gson.Gson;
 import com.seeu.R;
+import com.seeu.common.Constants;
 import com.seeu.member.Member;
 import com.seeu.team.Team;
 
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+
+import ua.naiksoftware.stomp.Stomp;
+import ua.naiksoftware.stomp.client.StompClient;
 
 /**
  * Created by thomasfouan on 06/05/2018.
@@ -26,6 +31,9 @@ public class ChatActivity extends ListActivity {
 
 	private MessageListAdapter messageListAdapter;
 	private List<Message> messages;
+
+	private StompClient stompClient;
+	private Gson gson = new Gson();
 
 	@Override
 	protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -41,6 +49,14 @@ public class ChatActivity extends ListActivity {
 
 		getInfoFromCaller();
 		loadMessages();
+		startWebSocketConfig();
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+
+		stompClient.disconnect();
 	}
 
 	private void getInfoFromCaller() {
@@ -66,18 +82,34 @@ public class ChatActivity extends ListActivity {
 		messageListAdapter.notifyDataSetChanged();
 	}
 
+	private void startWebSocketConfig() {
+		stompClient = Stomp.over(Stomp.ConnectionProvider.OKHTTP, Constants.SEEU_WEB_SOCKET_SERVER_URL);
+		stompClient.connect();
+
+		stompClient.topic("/topic/greetings").subscribe(topicMessage -> {
+			Message message = gson.fromJson(topicMessage.getPayload(), Message.class);
+			message.setOwner(Member.getDebugMember(0));
+			runOnUiThread(() -> addMessage(message));
+		});
+	}
+
 	public void onNewMessageClick(View v) {
 		String text = newMessage.getText().toString();
+		newMessage.getText().clear();
 
 		if (!text.isEmpty()) {
 			Message message = new Message();
-			message.setId(messages.size());
+//			message.setId(messages.size());
 			message.setContent(text);
+			// TODO: get current user
 			message.setOwner(Member.getDebugMember(0));
 
-			messages.add(message);
-			messageListAdapter.notifyDataSetChanged();
+			stompClient.send("/app/hello", gson.toJson(message)).subscribe();
 		}
-		newMessage.getText().clear();
+	}
+
+	private void addMessage(final Message message) {
+		messages.add(message);
+		messageListAdapter.notifyDataSetChanged();
 	}
 }
